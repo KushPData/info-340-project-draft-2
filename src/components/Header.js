b9c8d5b81ad14ca43d7d933da9606aef6bf352a8
Kush Patel <kushp03@uw.edu>
2023-05-19 12:41:55 -0700

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export function Header(props) {
+    return (
+        <header className="header-container">
+            <div className="header-item">
+                <img className="uw-logo" src="../../public/UW_Logo.png" alt="University of Washington logo" />
+            </div>
+
+            <div className="header-item">
+                <h1 className="heading">Husky Event Finder</h1>
+            </div>
+
+            <div className="header-item">
+                <img className="user-pic" src="../../public/user.jpg" alt="User Picture" />
+            </div>
+        </header>
+    );
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -45,5 +45,5 @@ export function Home(props) {
                 </div>
             </main>
         </div>
-    )
+    );
 }
