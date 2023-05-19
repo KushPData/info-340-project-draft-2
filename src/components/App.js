99423054bc655ae45a7f0fcceb1d8b44c2516973
Kush Patel <kushp03@uw.edu>
2023-05-19 12:31:50 -0700

    added Home.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App(props) {
+    return (
+        <div>
+            <Home />
+            {/* <Calendar /> */}
+            {/* <Upload /> */}
+            {/* <Review /> */}
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export function Home(props) {
+    return (
+        <div>
+            <Header />
+            <main>
+                <div className="main-box-lg">
+
+                    <div className="my-events">
+                        <h2>My Events</h2>
+                    </div>
+
+                    <nav className="lg-menu">
+                        <ul>
+                            <li><a href="#/">Home</a></li>
+                            <li><a href="#/">Event Calendar</a></li>
+                            <li><a href="#/">Upload Event</a></li>
+                            <li><a href="#/">Reviews</a></li>
+                        </ul>
+                    </nav>
+
+                    <div className="lg-search">
+                        <label className="search-key" for="search-id">Search: </label>
+                        <input id="search-id" className="search-engine" type="search" name="search" />
+                    </div>
+                </div>
+
+                <div className="search-box">
+                    <label for="search-id">Search: </label>
+                    <input id="search-id" className="search-engine" type="search" name="search" />
+                </div>
+
+
+                <div className="my-events-container">
+                    <div className="my-events">
+                        <h2>My Events</h2>
+                    </div>
+                </div>
+
+                <div className="upcoming-events-container">
+                    <div className="events-title">
+                        <h3>Upcoming Events</h3>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
