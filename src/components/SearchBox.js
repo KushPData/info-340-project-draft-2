4c1059da37be2e1bb5c7819904f2b093d054c96c
Kush Patel <kushp03@uw.edu>
2023-07-27 20:03:52 -0700

    added form.html

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function SearchBox(props) {
     return (
         <div className="search-box">
-            <label for="search-id">Search: </label>
+            <label htmlFor="search-id">Search: </label>
             <input id="search-id" className="search-engine" type="search" name="search" />
         </div>
     )
